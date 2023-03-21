e13467e1fa87881a219580c1c1f7d756b46ca749
Tukkos <jpfcastro@id.uff.br>
2023-03-21 11:24:09 -0300

    First commit

---
+++ src/App.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int[] numeroUmAoDez = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int i = 0;
+
+        Scanner ler = new Scanner(System.in);
+        System.out.println("Informe um número: ");
+        int numero = ler.nextInt();
+
+        System.out.println("Tabela de multiplicação de X");
+        while (i < numeroUmAoDez.length) {
+            System.out.println(numero + " x " + numeroUmAoDez[i] + " = " + (numeroUmAoDez[i] * numero));
+            i++;
+        }
+    }
+}
